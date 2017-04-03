6105464d8ff2fa5fe0c4c97e1bc99787b081e1a3
Andy <andrewrfindlay@ntlworld.com>
2017-04-03 14:17:44 +0100

    Added Arduino C++ and removed Arduino Plugin

---
+++ CrossBeam/Source/CrossBeam/CrossBeam.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CrossBeam.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CrossBeam, "CrossBeam" );
