a3a38b8742514dc36a3d90e865d9c169c1f6b7af
Andrew Findlay <andrewrfindlay@ntlworld.com>
2017-04-22 16:44:43 +0100

    Added arduino code

---
+++ TurretControllerSketch/TurretControllerSketch.ino
@@ -0,0 +1,54 @@
+const int turretPower[] = {13, 12};
+const int turretInput[] = {A0, A1};
+const int buttonPin = 3;
+int buttonState = 0;
+String outputValues[] = {"", "", "", ""};
+String output;
+int delayTime = 30;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  //turret inputs and power
+  for (int i = 0; i <2; ++i)
+  {
+  pinMode(turretPower[i], OUTPUT);
+  digitalWrite(turretPower[i], HIGH);
+  }
+
+  //button setup
+  pinMode(buttonPin, INPUT);
+}
+
+void loop()
+{
+  //Read pushbutton state
+  buttonState = digitalRead(buttonPin);
+
+  //Fire turrets on button press
+
+  for (int i = 0; i <2; ++i)
+    {
+    int turretValue = ((analogRead(turretInput[i])- 512)/-6);
+    int turretValueLength = (String(turretValue).length());
+  
+    outputValues[i] = String(turretValueLength) + 
+    ":" + turretValue;
+  
+    output = output + outputValues[i] + ",";
+    }
+
+  //Overwrite output if firing
+  if (buttonState == HIGH)
+    {
+      output = "F";
+    }
+
+  Serial.println(output);
+  
+  delay(delayTime);
+
+  //reset output and delay for next run
+  output = "";
+}
